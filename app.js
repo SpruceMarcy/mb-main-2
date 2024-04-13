829404044131bf2540d7becdc5508fb038eb0115
SpruceMarcy <mdr.brook@gmail.com>
2024-04-13 19:32:27 +0100

    First edition of KCAQO

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var createError = require('http-errors');
+var subdomain = require('express-subdomain');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -8,13 +9,21 @@ var indexRouter = require('./routes/index');
 var toolsRouter = require('./routes/tools');
 var shenanigansRouter = require('./routes/shenanigans')
 var projectsRouter = require('./routes/projects')
+var kcaqo = require('./routes/kcaqo')
 
 var app = express();
+var defaultRouter = express.Router();
+
 
 // view engine setup
 app.engine('ejs', require('ejs-mate'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+if(process.env.ENVIRONMENT_SPEC -= "prod") {
+  app.set('subdomain offset',3)
+} else {
+  app.set('subdomain offset',1)
+}
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,12 +31,29 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/tools', toolsRouter);
-app.use('/shenanigans', shenanigansRouter);
-app.use('/shenans', shenanigansRouter);
-app.use('/nans',shenanigansRouter)
-app.use('/projects',projectsRouter)
+
+kcaqo.get('*', function(req, res, next) {
+  if(req.url.startsWith('/fonts')) {
+    next()
+  } else if(req.url.startsWith('/images')) {
+    next()
+  } else {
+    next(createError(404));
+  }
+})
+
+defaultRouter.use('/', indexRouter);
+defaultRouter.use('/tools', toolsRouter);
+defaultRouter.use('/shenanigans', shenanigansRouter);
+defaultRouter.use('/shenans', shenanigansRouter);
+defaultRouter.use('/nans',shenanigansRouter)
+defaultRouter.use('/projects',projectsRouter)
+
+
+app.use(subdomain("KeepCalmAndQueerOn",kcaqo))
+
+app.use(subdomain("keepcalmandqueeron",kcaqo))
+app.use(defaultRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,7 +61,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+defaultRouter.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ routes/kcaqo.js
@@ -0,0 +1,16 @@
+let express = require('express');
+let router = express.Router();
+
+let latestIssue = '1'
+
+router.get('/', function(req, res, next) {
+  res.redirect(303, '/issue/'+latestIssue);
+});
+router.get('/issue/:issue/?', function(req, res, next) {
+  let issueNo = Number(req.params.issue)
+  if (!issueNo || issueNo < 1 || issueNo > latestIssue){
+    res.redirect(303, '/issue/'+latestIssue);
+  }
+  res.render('kcaqo/show', {'issue':req.params.issue});
+});
+module.exports = router;
