d7d512981536d191615f6f3c6da11415285a2865
SpruceMarcy <mdr.brook@gmail.com>
2024-06-09 00:11:07 +0100

    Issue #9

--- routes/kcaqo.js
+++ routes/kcaqo.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let router = express.Router();
 
-let latestIssue = '8'
+let latestIssue = '9'
 
 router.get('/', function(req, res, next) {
   res.redirect(303, '/issue/'+latestIssue);
